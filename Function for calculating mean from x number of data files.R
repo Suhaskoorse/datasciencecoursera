66b11a4780b037297c8ba54e68ee7e9a2bff278e
Suhas koorse <suhaskoorse@outlook.com>
2019-04-28 15:18:14 -0700

    add two more function calls

--- Function for calculating mean from x number of data files.R
+++ Function for calculating mean from x number of data files.R
@@ -15,3 +15,5 @@ pollutantmean <-function(directory,pollutant, id)
   
 }
 pollutantmean("specdata","sulfate",1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "nitrate", 23)
